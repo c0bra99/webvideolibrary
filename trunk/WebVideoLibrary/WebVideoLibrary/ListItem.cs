19c552aacf26b5de15c8715d706a2b9e2e048111
c0bra99 <c0bra99@22da3876-bb5f-11dd-9405-3d0920048215>
2008-12-01 04:02:01 +0000

    Added ListItem class

---
+++ trunk/WebVideoLibrary/WebVideoLibrary/ListItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebVideoLibrary
+{
+    /// <summary>
+    /// A simple class for representing a name value pair in a combo box.
+    /// </summary>
+    class ListItem
+    {
+        public string Name { get; set; }
+        public object Value { get; set; }
+
+        public ListItem(string name, object value)
+        {
+            Name = name;
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
