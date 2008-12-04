d5575442785ff2dc0f9ec7a4969fcaa18698ed85
c0bra99@gmail.com <c0bra99@gmail.com@22da3876-bb5f-11dd-9405-3d0920048215>
2008-12-04 00:49:51 +0000

    Made it work correctly with the database

--- WebVideoLibrary/DataLayer/ClipAttribute.cs
+++ WebVideoLibrary/DataLayer/ClipAttribute.cs
@@ -13,7 +13,9 @@ namespace DataLayer
         public string Value { get; set; }
 
         public ClipAttribute()
-        { }
+        {
+            ID = -1;
+        }
 
         public ClipAttribute(int id, int clipID, string name, string value)
         {
@@ -28,25 +30,40 @@ namespace DataLayer
         /// </summary>
         public void Save()
         {
-            using (SQLiteConnection cnn = new SQLiteConnection(Utility.GetConnectionString()))
+            using (SQLiteConnection cnn = new SQLiteConnection(Utility.CONNECTION_STRING))
             {
                 using (SQLiteCommand cmd = cnn.CreateCommand())
                 {
                     cnn.Open();
                     cmd.CommandText = "INSERT INTO ClipAttributes(ID, ClipID, Name, Value) VALUES(?, ?, ?, ?)";
                     cmd.Parameters.Add("ID", DbType.Int32);
-                    cmd.Parameters["ID"].Value = ID;
+                    if (ID != -1)
+                    {
+                        cmd.Parameters["ID"].Value = ID;
+                    }
                     cmd.Parameters.Add("ClipID", DbType.Int32);
                     cmd.Parameters["ClipID"].Value = ClipID;
                     cmd.Parameters.Add("Name", DbType.String);
                     cmd.Parameters["Name"].Value = Name;
                     cmd.Parameters.Add("Value", DbType.String);
-                    cmd.Parameters["Value"].Value = Name;
+                    cmd.Parameters["Value"].Value = Value;
 
                     int rowsUpdated = cmd.ExecuteNonQuery();
                     if (rowsUpdated != 1)
                     {
                         throw new SQLiteException("Too many or too little rows updated! Expected 1, Updated: " + rowsUpdated);
+                    }
+                }
+                if (ID == -1)
+                {
+                    using (SQLiteCommand cmd = cnn.CreateCommand())
+                    {
+                        if (cnn.State != ConnectionState.Open)
+                        {
+                            cnn.Open();
+                        }
+                        cmd.CommandText = "SELECT last_insert_rowid();";
+                        ID = (int)(long)cmd.ExecuteScalar();
                     }
                 }
             }
--- WebVideoLibrary/DataLayer/Utility.cs
+++ WebVideoLibrary/DataLayer/Utility.cs
@@ -7,14 +7,8 @@ namespace DataLayer
 {
     public static class Utility
     {
-        public static string GetDatabasePathAndFile()
-        {
-            return @"c:\database.db3";
-        }
-
-        public static string GetConnectionString()
-        {
-            return @"Data Source=" + GetDatabasePathAndFile();
-        }
+        public const string DATABASE_FILENAME = @"database.s3db";
+        public const string DATABASE_FILENAME_AND_PATH = @"c:\" + DATABASE_FILENAME;
+        public const string CONNECTION_STRING = @"Data Source=" + DATABASE_FILENAME_AND_PATH;
     }
 }
